d77ed6a11d166635c4992c0a697b839c848298cf
BelleZhou <xuczhou@ucdavis.edu>
2019-07-12 11:40:23 -0700

    Update Invader.java
    new interface

--- app/src/main/java/com/nike/spaceinvaders/Invader.java
+++ app/src/main/java/com/nike/spaceinvaders/Invader.java
@@ -1,7 +1,37 @@
 package com.nike.spaceinvaders;
+import java.io.*;
 
 /**
  * Developer Henry Yi
+ * Developer Pathac
  */
-class Invader {
+
+interface Invader
+{
+    // public and abstract
+
+    /*void display();
+    int getposx();
+    int getposy();
+    int gethealth();
+    void move();
+    void fire();
+    void injured();
+    void death();
+    */
+}
+
+class InvaderT1 implements Invader{
+    public InvaderT1(){
+    };
+}
+
+class InvaderT2 implements Invader{
+    public InvaderT2(){
+    };
+}
+
+class UFO implements Invader{
+    public UFO(){
+    };
 }
