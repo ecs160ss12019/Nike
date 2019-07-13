cdad902a0edc33cd0eb4f9c3f4bbc660f3849e06
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2019-07-12 17:17:15 -0700

    framework on BaseShelter

--- app/src/main/java/com/nike/spaceinvaders/SpaceGame.java
+++ app/src/main/java/com/nike/spaceinvaders/SpaceGame.java
@@ -3,8 +3,11 @@
 package com.nike.spaceinvaders;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
