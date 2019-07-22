9ff93a363180c67c7f61a0ddefb10bd9815ff452
CharlesYWL <xlyin@ucdavis.edu>
2019-07-22 00:15:48 -0700

    Framework on HUD

--- app/src/main/java/com/nike/spaceinvaders/HUD.java
+++ app/src/main/java/com/nike/spaceinvaders/HUD.java
@@ -5,8 +5,10 @@ import android.graphics.PointF;
 import android.os.Handler;
 import android.support.annotation.IdRes;
 import android.support.constraint.ConstraintLayout;
+import android.util.Pair;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -27,8 +29,26 @@ public class HUD extends AnimatedObject <ConstraintLayout> {
     }
 
     @Override
-    protected void handle(Actions actions, Set keys) {
+    protected void handle(Actions actions) {
+        Set<Integer> keys=actions.keySet();
+        for (Integer key: keys){
+            Pair<AnimatedObject, ArrayList<Float>> value=actions.get(key);
+            switch (key){
+                case SpaceGame.LIFE_ADD:
+                    regen();
+                    break;
+                case SpaceGame.LIFE_GONE:
+                    hurt();
+                    break;
+                default: return;
+            }
+        }
+    }
 
+    private void regen() {
+
+    }
+    private void hurt() {
     }
 
     @Override
