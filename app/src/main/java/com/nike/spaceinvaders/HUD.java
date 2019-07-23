5698393cdd3bfa66a5a0c65ce8b3200e44824487
CharlesYWL <xlyin@ucdavis.edu>
2019-07-22 22:25:07 -0700

    clean useless function in HUD

--- app/src/main/java/com/nike/spaceinvaders/HUD.java
+++ app/src/main/java/com/nike/spaceinvaders/HUD.java
@@ -13,84 +13,37 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Set;
 
 public class HUD extends AnimatedObject <ConstraintLayout> implements StatusManager {
     private TextView score;
-    private ConstraintLayout LiveLayout;
-    private ImageView live1,live2,live3;
-    private Lives lives;
+    private Lives liveLayout;
+    private int lives;
     private static int LIVEMAX=3;
-    private int scoreNum;
 
 
     @IdRes
     private final int scoreId=R.id.score;
     @IdRes
     private final int livesId=R.id.lives;
-    @IdRes
-    private final int live1Id=R.id.live1;
-    @IdRes
-    private final int live2Id=R.id.live2;
-    @IdRes
-    private final int live3Id=R.id.live3;
 
 
     HUD(ConstraintLayout view, SpaceGame.Resources resources, SpaceGame spaceGame, SpaceGame.Status status, Handler mainHandler, Handler processHandler) {
         super(null, view, resources, spaceGame,status, mainHandler, processHandler);
         //init
-        lives = new Lives(view,resources,spaceGame,status,mainHandler,processHandler,LIVEMAX);
+        this.lives=this.LIVEMAX;
         this.score=view.findViewById(scoreId);
-        this.LiveLayout=view.findViewById(livesId);
-        this.live1=view.findViewById(live1Id);
-        this.live2=view.findViewById(live2Id);
-        this.live3=view.findViewById(live3Id);
-        scoreNum=0;
+        this.liveLayout=new Lives((ConstraintLayout) view.findViewById(livesId),resources,spaceGame,status,mainHandler,processHandler,LIVEMAX);
     }
 
     @Override
 
     protected void handle(Actions actions,Set<Integer> keys) {
-        for (Integer key: keys){
-            Pair<AnimatedObject, SparseArray<Float>> value=actions.get(key);
-            switch (key){
-                case SpaceGame.TEST: //Test only
-                    score.setText(String.valueOf(scoreNum));
-                    updateLives();
-                    break;
-                case SpaceGame.LIFE_ADD: //when we gain live
-                    lives.handle(actions,keys);
-                    updateLives();
-                    break;
-                case SpaceGame.LIFE_GONE: //when we lose live
-                    lives.handle(actions,keys);
-                    updateLives();
-                    break;
-                default: return;
-            }
-        }
     }
 
     private void updateLives() {
-        int livenum = lives.getLives();
-        switch (livenum){
-            case 1:
-                live1.setVisibility(TextView.VISIBLE);
-                live2.setVisibility(TextView.INVISIBLE);
-                live3.setVisibility(TextView.INVISIBLE);
-                break;
-            case 2:
-                live1.setVisibility(TextView.VISIBLE);
-                live2.setVisibility(TextView.VISIBLE);
-                live3.setVisibility(TextView.INVISIBLE);
-                break;
-            case 3:
-                live1.setVisibility(TextView.VISIBLE);
-                live2.setVisibility(TextView.VISIBLE);
-                live3.setVisibility(TextView.VISIBLE);
-                break;
-                default: return;
-        }
+
     }
 
 
@@ -101,7 +54,21 @@ public class HUD extends AnimatedObject <ConstraintLayout> implements StatusMana
 
     @Override
     public void updateStatus(SpaceGame.Status status) {
-        float score = status.get(SpaceGame.SCORES).first;
-        this.score.setText(String.valueOf(score));
+        Float livesTemp=(Objects.requireNonNull(status.get(SpaceGame.NUM_LIVES)).first);
+        int lives=livesTemp.intValue();
+        Actions actions=new Actions();
+
+        for(int index =0; index<Math.abs(lives-this.lives);index++){
+            if (lives-this.lives>0){
+                actions.put(SpaceGame.LIFE_ADD,null);
+                this.liveLayout.handle(actions);
+            }else if (lives-this.lives<0){
+                actions.put(SpaceGame.LIFE_GONE,null);
+                this.liveLayout.handle(actions);
+            }
+
+        }
+        this.lives=lives;
+
     }
 }
