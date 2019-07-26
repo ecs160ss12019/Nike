1eb4979b5506eae8a6877fbc9174c169902e9066
BelleZhou <xuczhou@ucdavis.edu>
2019-07-25 20:22:14 -0700

    Making the Mystery Invader

---
+++ app/src/main/java/com/nike/spaceinvaders/MysteryInvader.java
@@ -0,0 +1,105 @@
+package com.nike.spaceinvaders;
+
+import android.animation.ValueAnimator;
+import android.content.res.Resources;
+import android.graphics.PointF;
+import android.util.ArraySet;
+import android.util.Log;
+import android.util.Pair;
+import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Set;
+
+import android.os.Handler;
+
+
+public class MysteryInvader extends AnimatedObject <ImageView>{
+    public boolean alive = true;
+    private boolean status=true;
+    private int[][] hitbox;
+    private int index;
+    private int appearcd;
+    private Random rand;
+
+    MysteryInvader(int index,ValueAnimator animator, ImageView view, SpaceGame.Resources resources, SpaceGame spaceGame, SpaceGame.Status status, Handler mainHandler, Handler processHandler) {
+        super(animator, view, resources, spaceGame, status,mainHandler, processHandler);
+        this.index=index;
+        rand = new Random();
+        appearcd = 50+rand.nextInt(1000);
+    }
+
+    private void kill(Actions actions,AnimatedObject missile){
+        alive = false;
+        this.status=false;
+        this.setVisibility(View.INVISIBLE);
+        Set<Integer> keys=new ArraySet<>();
+        keys.add(SpaceGame.MISSILE_GONE);
+        keys.add(SpaceGame.HIT);
+        actions.put(SpaceGame.MISSILE_GONE,null);
+        actions.put(SpaceGame.HIT,new Pair<>(this,null));
+        missile.handle(actions,keys);
+        this.getSpaceGame().invaderGroup.handle(actions,keys);
+        notifySpaceGame();
+    }
+
+    private void notifySpaceGame(){
+        SpaceGame.Status status=getStatus();
+        Pair<Float,Float> value=status.get(SpaceGame.SCORES);
+        assert value != null;
+        status.put(SpaceGame.SCORES,new Pair<>(value.first+10,null));
+        getSpaceGame().updateStatus(status);
+    }
+
+    private boolean hitDetection(Actions actions,AnimatedObject missile){
+        if (!status){
+            return false;
+        }
+        float x=missile.getX();
+        float y=missile.getY();
+        int missileWidth=missile.getWidth();
+        float left,top,bottom,right;
+        left=this.getAbsoluteX()+50;
+        top=this.getAbsoluteY();
+        bottom=top+this.getHeight();
+        right=left+this.getWidth()-50;
+        if ((x>=left&&x<=right&&y<=bottom&&y>=top)||((x+missileWidth)>=left&&(x+missileWidth)<=right&&y<=bottom&&y>=top)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void handle(Actions actions, Set<Integer> keys) {
+        //Log.d("invader handle","in handle");
+        for (Integer key: keys){
+            Pair<AnimatedObject, SparseArray<Float>> value=actions.get(key);
+
+            switch (key){
+                case SpaceGame.STRIKE:
+                    assert value != null;
+                    if (hitDetection(actions,value.first)){
+                        kill(actions,value.first);
+                    }
+
+            }
+        }
+    }
+
+    @Override
+    ValueAnimator.AnimatorUpdateListener animatorListenerConfigure() {
+        return new ValueAnimator.AnimatorUpdateListener(){
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+            }
+        };
+    }
+
+
+}
