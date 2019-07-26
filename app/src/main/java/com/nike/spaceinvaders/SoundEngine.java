3333c35895328fd9cdbb05578e28cc8da4d5f316
Roberto Lozano <rlozano@ucdavis.edu>
2019-07-26 15:43:33 -0700

    update SoundEngine
    updates to soundengine, member variables and constructor

--- app/src/main/java/com/nike/spaceinvaders/SoundEngine.java
+++ app/src/main/java/com/nike/spaceinvaders/SoundEngine.java
@@ -8,5 +8,54 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 
-public class SoundEngine {
+import java.io.IOException;
+
+class SoundEngine {
+    // for playing sound effects
+    private static SoundPool mSP;
+    private static int mJump_ID = -1;
+    private static int mReach_Objective_ID = -1;
+    private static  int mCoin_Pickup_ID = -1;
+    private static  int mPlayer_Burn_ID = -1;
+
+    private static SoundEngine ourInstance;
+
+    public SoundEngine(Context c){
+        // Initialize the SoundPool
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            mSP = new SoundPool.Builder()
+                    .setMaxStreams(5)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            mSP = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        }
+        try {
+            AssetManager assetManager = c.getAssets();
+            AssetFileDescriptor descriptor;
+
+            // Prepare the sounds in memory
+            descriptor = assetManager.openFd("jump.ogg");
+            mJump_ID = mSP.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("reach_objective.ogg");
+            mReach_Objective_ID = mSP.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("coin_pickup.ogg");
+            mCoin_Pickup_ID = mSP.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("player_burn.ogg");
+            mPlayer_Burn_ID = mSP.load(descriptor, 0);
+
+        } catch (IOException e) {
+            // Error
+        }
+
+    }
+
 }
