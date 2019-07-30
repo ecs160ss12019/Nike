6cccc20241ff1d7b95ecf8f06e761afdd05352a9
Roberto Lozano <rlozano@ucdavis.edu>
2019-07-30 15:24:28 -0700

    Update MusicPlayer.java
    Updates to musicPlayer

--- app/src/main/java/com/nike/spaceinvaders/MusicPlayer.java
+++ app/src/main/java/com/nike/spaceinvaders/MusicPlayer.java
@@ -1,4 +1,44 @@
 package com.nike.spaceinvaders;
 
-public class MusicPlayer {
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MusicPlayer extends Service implements MediaPlayer.OnErrorListener {
+
+    private final IBinder mBinder = new ServiceBinder();
+    MediaPlayer mPlayer;
+    private int length = 0;
+
+
+    public MusicPlayer() {
+    }
+
+    public class ServiceBinder extends Binder {
+        MusicPlayer getService() {
+            return MusicPlayer.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return mBinder;
+    }
+
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+
+        Toast.makeText(this, "Music player failed", Toast.LENGTH_SHORT).show();
+        if (mPlayer != null) {
+            try {
+                mPlayer.stop();
+                mPlayer.release();
+            } finally {
+                mPlayer = null;
+            }
+        }
+        return false;
+    }
 }
